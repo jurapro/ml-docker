4c3a679a95f6d41069d2890f3dec6378ed4666b2
jurapro <jurapro@yandex.ru>
2022-09-20 21:21:51 +0700

    adding auth service

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,35 +1,19 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const {connectDB} = require("./helpers/db");
 const {host, port, db} = require("./configuration");
 
 
-const postSchema = new mongoose.Schema({
-    name: String
-});
-const Post = mongoose.model('Post', postSchema);
-
 const startServer = () => {
     app.listen(port, async () => {
-        console.log(`Starting api service on port: ${port}`);
+        console.log(`Starting auth service on port: ${port}`);
         console.log(`On host: ${host}`);
         console.log(`Our database: ${db}`);
-
-/*        const silence = new Post({name: 'Silence'});
-        console.log(silence.name);
-
-        await silence.save();
-        const kittens = await Post.find();
-
-        console.log(kittens);*/
-
-
     })
 }
 
 app.get("/test", (req, res) => {
-    res.send("Api service working!");
+    res.send("Auth service working!");
 })
 
 connectDB()
